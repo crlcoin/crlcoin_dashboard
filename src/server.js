f385246b22399bb51f411b92840ff49defb491db
Vinicius Cosmo <viniciuscosmosp@gmail.com>
2021-10-04 07:48:01 -0300

    Adicionado o cors

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const path = require('path')
 const session = require('express-session')
+const cors = require('cors')
 const app = express()
 const routes = require('./routes')
 const handlebarsHelpers = require('./helper/handlebars')
@@ -15,12 +16,13 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         secure: IS_PRODUCTION,
-        maxAge: 1000 * 60 * 60 * 24,
+        maxAge: 1000 * 60 * 60 * 3,
     }
 }))
 
+app.use(cors())
 app.use( express.json() )
-app.use( express.urlencoded({ extended: false }) )
+app.use( express.urlencoded({ extended: true }) )
 app.use( express.static( path.join(__dirname, '../public/') ) )
 
 app.engine( 'handlebars', handlebars({
